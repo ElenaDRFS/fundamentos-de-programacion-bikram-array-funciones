b59afa0f5516971a84df8f4b5d3bc09fb46ab01a
ElenaDRFS <elenafullstack@gmail.com>
2023-09-25 22:15:05 +0200

    inicio

---
+++ index.js
@@ -0,0 +1,23 @@
+let arrayVacio = [];
+let arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arrayNumerosPares = [0, 2, 4, 6, 8];
+let arrayBidimensional = [[0, 1, 2], ['a', 'b', 'c']];
+
+function suma(num1,num2){
+    return num1 + num2;
+} 
+suma(52,36);
+
+function potenciacion(a,b){
+    return Math.pow(a,b);
+}
+potenciacion(10,0);
+
+function separarPalabras(){
+    return Array.from(); 
+
+}
+separarPalabras('Hola que tal?')
+
+
+
